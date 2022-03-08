e01f8dac7392eb5577f79500d813e6e76ec5fd9a
Devon Wigle <devonwigle@gmail.com>
2022-03-08 10:27:36 -0600

    Attach api to App

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,11 @@
 export const getOrders = () => {
   return fetch('http://localhost:3001/api/v1/orders')
-      .then(response => response.json())
+      .then(response => errorCheck(response))
+}
+const errorCheck = (response) => {
+  if (!response.ok) {
+    throw new Error("Could not retrieve orders")
+  } else {
+    return response.json()
+  }
 }
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -14,6 +14,8 @@ class App extends Component {
 
   componentDidMount() {
     getOrders()
+    .then(data => {this.setState({orders: data.orders})
+    })
       .catch(err => console.error('Error fetching:', err));
   }
 
