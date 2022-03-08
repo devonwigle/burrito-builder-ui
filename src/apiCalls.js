a6c508fd81e5d0ff22ab3958e96766e7a449abc7
Devon Wigle <devonwigle@gmail.com>
2022-03-08 11:49:29 -0600

    Add post api call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,6 +2,23 @@ export const getOrders = () => {
   return fetch('http://localhost:3001/api/v1/orders')
       .then(response => errorCheck(response))
 }
+
+export const postOrder = (newOrder) => {
+  return fetch('http://localhost:3001/api/v1/orders', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(order),
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error("Failed to post order")
+    }
+    return response.json()
+  })
+}
+
 const errorCheck = (response) => {
   if (!response.ok) {
     throw new Error("Could not retrieve orders")
